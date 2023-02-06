073ec56129ec6d4fd32e45f515a7cae7adf092d4
HediehKh <113653779+HediehKh@users.noreply.github.com>
2023-02-06 10:39:03 -0800

    feb6

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Survey from './Survey'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+  
+      </Head>
+      <main className={styles.main}>
+        <Survey/>
+
+      
+      </main>
+    </>
+  )
+}
